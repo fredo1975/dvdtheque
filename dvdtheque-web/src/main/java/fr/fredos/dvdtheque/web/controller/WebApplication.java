f4f0714aa906e96512ba15f722d5e0534365abd9
fredo <fredo@BLUECHIPIT-1>
2018-11-01 19:05:43 +0100

    add Dockerfile

--- dvdtheque-web/src/main/java/fr/fredos/dvdtheque/web/controller/WebApplication.java
+++ dvdtheque-web/src/main/java/fr/fredos/dvdtheque/web/controller/WebApplication.java
@@ -2,8 +2,11 @@ package fr.fredos.dvdtheque.web.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
-@SpringBootApplication(scanBasePackages = "fr.fredos.dvdtheque.web")
+@SpringBootApplication(scanBasePackages = "fr.fredos.dvdtheque")
+@PropertySource("classpath:application.properties")
+//@EnableAutoConfiguration
 public class WebApplication {
 
 	public static void main(String... args) {
