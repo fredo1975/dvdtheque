93097b816b6c07b30200249cb6eb9acf9478d2dc
fredo <fredo@BLUECHIPIT-1>
2019-04-01 00:19:03 +0200

    add new rest service

--- dvdtheque-tmdb-service/src/main/java/fr/fredos/dvdtheque/tmdb/service/FilmRestService.java
+++ dvdtheque-tmdb-service/src/main/java/fr/fredos/dvdtheque/tmdb/service/FilmRestService.java
@@ -1,4 +1,4 @@
-package fr.fredos.dvdtheque.swing.service;
+package fr.fredos.dvdtheque.tmdb.service;
 
 import java.util.LinkedHashMap;
 import java.util.List;
---
+++ dvdtheque-tmdb-service/src/test/java/fr/fredos/dvdtheque/dvdtheque/tmdb/service/test/FilmRestServiceTests.java
@@ -0,0 +1,42 @@
+package fr.fredos.dvdtheque.dvdtheque.tmdb.service.test;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import fr.fredos.dvdtheque.dao.model.object.Film;
+import fr.fredos.dvdtheque.service.IFilmService;
+import fr.fredos.dvdtheque.service.IPersonneService;
+import fr.fredos.dvdtheque.tmdb.service.FilmRestService;
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {fr.fredos.dvdtheque.dao.Application.class,
+		fr.fredos.dvdtheque.service.ServiceApplication.class,
+		fr.fredos.dvdtheque.tmdb.service.TmdbServiceApplication.class})
+public class FilmRestServiceTests extends AbstractTransactionalJUnit4SpringContextTests{
+	protected Logger logger = LoggerFactory.getLogger(FilmRestServiceTests.class);
+	@Autowired
+	protected IFilmService filmService;
+	@Autowired
+	protected IPersonneService personneService;
+	@Autowired
+	protected FilmRestService filmRestService;
+	
+	@Test
+	public void findAllFilmsRestService() {
+		List<Film> dbFilmList = filmService.findAllFilms();
+		assertNotNull(dbFilmList);
+		
+		List<LinkedHashMap<String, Object>> restFilmList = filmRestService.findAllFilms();
+		assertNotNull(restFilmList);
+	}
+}
