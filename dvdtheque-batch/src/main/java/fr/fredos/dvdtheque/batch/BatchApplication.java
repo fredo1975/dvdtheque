2ea050038268d969008b7930ebb02f04d3869462
fredo <fredo@BLUECHIPIT-1>
2019-06-29 22:05:38 +0200

    remove unused logger

--- dvdtheque-batch/src/main/java/fr/fredos/dvdtheque/batch/BatchApplication.java
+++ dvdtheque-batch/src/main/java/fr/fredos/dvdtheque/batch/BatchApplication.java
@@ -1,7 +1,5 @@
 package fr.fredos.dvdtheque.batch;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 		"fr.fredos.dvdtheque.dao",
 		"fr.fredos.dvdtheque.tmdb.service"})
 public class BatchApplication {
-	protected Logger logger = LoggerFactory.getLogger(BatchApplication.class);
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(BatchApplication.class, args);
     }
