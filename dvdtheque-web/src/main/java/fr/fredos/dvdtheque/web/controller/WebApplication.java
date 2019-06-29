56ffcbba7ea489990c55b7c79e4d83c2ea571b23
fredo <fredo@BLUECHIPIT-1>
2019-06-29 22:07:45 +0200

    remove comment

--- dvdtheque-web/src/main/java/fr/fredos/dvdtheque/web/controller/WebApplication.java
+++ dvdtheque-web/src/main/java/fr/fredos/dvdtheque/web/controller/WebApplication.java
@@ -2,11 +2,8 @@ package fr.fredos.dvdtheque.web.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication(scanBasePackages = "fr.fredos.dvdtheque")
-@PropertySource("classpath:application.properties")
-//@EnableAutoConfiguration
 public class WebApplication {
 
 	public static void main(String... args) {
