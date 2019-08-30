83c71223b1a977317e651246d687dbd2b5e368e7
fredo1975 <fredo@BLUECHIPIT-1>
2019-08-30 22:14:37 +0200

    add new spring config client

---
+++ dvdtheque-config-client/src/test/java/fr/fredos/dvdtheque/config/client/DvdthequeConfigClientApplicationTests.java
@@ -0,0 +1,15 @@
+package fr.fredos.dvdtheque.config.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DvdthequeConfigClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+}
