942cb683ffd4092eba1fd017a551597aab808355
fredo1975 <fredo@BLUECHIPIT-1>
2019-09-02 22:36:52 +0200

    add @EnableDiscoveryClient

--- dvdtheque-web/src/main/java/fr/fredos/dvdtheque/web/controller/WebApplication.java
+++ dvdtheque-web/src/main/java/fr/fredos/dvdtheque/web/controller/WebApplication.java
@@ -2,8 +2,10 @@ package fr.fredos.dvdtheque.web.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = "fr.fredos.dvdtheque")
+@EnableDiscoveryClient
 public class WebApplication {
 
 	public static void main(String... args) {
