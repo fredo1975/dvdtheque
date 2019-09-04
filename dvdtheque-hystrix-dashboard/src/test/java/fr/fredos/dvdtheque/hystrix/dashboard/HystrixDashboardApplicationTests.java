82bdad24779fc3562656450114b0b8f5e669e40b
fredo1975 <fredo@BLUECHIPIT-1>
2019-09-04 23:48:43 +0200

    add hystrix dashboard

---
+++ dvdtheque-hystrix-dashboard/src/test/java/fr/fredos/dvdtheque/hystrix/dashboard/HystrixDashboardApplicationTests.java
@@ -0,0 +1,10 @@
+package fr.fredos.dvdtheque.hystrix.dashboard;
+
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+public class HystrixDashboardApplicationTests {
+	@Test
+    void contextLoads() {
+    }
+}
