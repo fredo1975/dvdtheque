87eb76ab2cf473c577c463c2fc843f029c48ab70
fredo1975 <fredo@BLUECHIPIT-1>
2019-09-09 00:22:39 +0200

    fix discovery server url service

--- dvdtheque-hystrix-dashboard/src/test/java/fr/fredos/dvdtheque/hystrix/dashboard/HystrixDashboardApplicationTests.java
+++ dvdtheque-hystrix-dashboard/src/test/java/fr/fredos/dvdtheque/hystrix/dashboard/HystrixDashboardApplicationTests.java
@@ -1,6 +1,7 @@
 package fr.fredos.dvdtheque.hystrix.dashboard;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class HystrixDashboardApplicationTests {
