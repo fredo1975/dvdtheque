3431a61b3cae31dfde01fe08bd81ecb70a2964ae
fredo1975 <fredo@BLUECHIPIT-1>
2019-09-14 10:39:28 +0200

    new input path injection

--- dvdtheque-batch/src/test/java/fr/fredos/dvdtheque/batch/BatchImportFilmsConfigurationTest.java
+++ dvdtheque-batch/src/test/java/fr/fredos/dvdtheque/batch/BatchImportFilmsConfigurationTest.java
@@ -15,7 +15,6 @@ import org.junit.Test;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
