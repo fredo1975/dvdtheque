201d34ea226cf4c54a177ca0a5ce76491cbf0b7a
fredo1975 <fredo@BLUECHIPIT-1>
2019-10-20 16:40:12 +0200

    add admin server

--- dvdtheque-discovery-server/src/main/java/fr/fredos/dvdtheque/discovery/server/DiscoveryServerApplication.java
+++ dvdtheque-discovery-server/src/main/java/fr/fredos/dvdtheque/discovery/server/DiscoveryServerApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
 @SpringBootApplication
 @EnableEurekaServer
+@EnableAdminServer
 public class DiscoveryServerApplication {
 	public static void main(String[] args) {
         SpringApplication.run(DiscoveryServerApplication.class, args);
