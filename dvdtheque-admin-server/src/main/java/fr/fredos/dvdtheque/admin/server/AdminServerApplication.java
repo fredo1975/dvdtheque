29574ddcd5e348c5db1db8dccc123c63923b888e
fredo1975 <fredo@BLUECHIPIT-1>
2019-10-20 23:43:20 +0200

    add admin server

---
+++ dvdtheque-admin-server/src/main/java/fr/fredos/dvdtheque/admin/server/AdminServerApplication.java
@@ -0,0 +1,29 @@
+package fr.fredos.dvdtheque.admin.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableAdminServer
+public class AdminServerApplication {
+	public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+
+    @Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().permitAll()  
+                .and().csrf().disable();
+        }
+    }
+}
--- dvdtheque-discovery-server/src/main/java/fr/fredos/dvdtheque/discovery/server/DiscoveryServerApplication.java
+++ dvdtheque-discovery-server/src/main/java/fr/fredos/dvdtheque/discovery/server/DiscoveryServerApplication.java
@@ -4,11 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
-
 @SpringBootApplication
 @EnableEurekaServer
-@EnableAdminServer
 public class DiscoveryServerApplication {
 	public static void main(String[] args) {
         SpringApplication.run(DiscoveryServerApplication.class, args);
