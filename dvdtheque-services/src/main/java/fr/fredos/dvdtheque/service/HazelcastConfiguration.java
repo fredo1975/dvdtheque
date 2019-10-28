af1678a3c1346165ed1fd2f38e6fe2d1599f1a55
fredo1975 <fredo@BLUECHIPIT-1>
2019-10-28 23:45:59 +0100

    add hazelcast cache manager

---
+++ dvdtheque-services/src/main/java/fr/fredos/dvdtheque/service/HazelcastConfiguration.java
@@ -0,0 +1,20 @@
+package fr.fredos.dvdtheque.service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.EvictionPolicy;
+import com.hazelcast.config.MapConfig;
+import com.hazelcast.config.MaxSizeConfig;
+
+@Configuration
+public class HazelcastConfiguration {
+	@Bean
+	public Config hazelCastConfig() {
+		return new Config().setInstanceName("hazelcast-instance")
+				.addMapConfig(new MapConfig().setName("films")
+						.setMaxSizeConfig(new MaxSizeConfig(200, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
+						.setEvictionPolicy(EvictionPolicy.LRU).setTimeToLiveSeconds(20));
+	}
+}
