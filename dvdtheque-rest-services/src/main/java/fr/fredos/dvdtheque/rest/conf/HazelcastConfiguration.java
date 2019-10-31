045f8369f7c1f58dbc6d80c916d96d9e9ea8389f
fredo1975 <fredo@BLUECHIPIT-1>
2019-11-01 00:24:38 +0100

    fix

--- dvdtheque-rest-services/src/main/java/fr/fredos/dvdtheque/rest/conf/HazelcastConfiguration.java
+++ dvdtheque-rest-services/src/main/java/fr/fredos/dvdtheque/rest/conf/HazelcastConfiguration.java
@@ -1,4 +1,4 @@
-package fr.fredos.dvdtheque.service;
+package fr.fredos.dvdtheque.rest.conf;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -8,15 +8,13 @@ import com.hazelcast.config.Config;
 import com.hazelcast.config.EvictionPolicy;
 import com.hazelcast.config.MapConfig;
 import com.hazelcast.config.MaxSizeConfig;
-
 @Configuration
 public class HazelcastConfiguration {
-	@Value("${hazelcast.group.name}")
-	private String groupConfigName;
+	
 	@Bean
 	public Config hazelCastConfig() {
 		Config config = new Config();
-		config.getGroupConfig().setName(groupConfigName);
+		config.getGroupConfig().setName("hazelcast-dev");
 		config.setInstanceName("hazelcast-instance").addMapConfig(new MapConfig().setName("films")
 				.setMaxSizeConfig(new MaxSizeConfig(200, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
 				.setEvictionPolicy(EvictionPolicy.LRU).setTimeToLiveSeconds(20));
