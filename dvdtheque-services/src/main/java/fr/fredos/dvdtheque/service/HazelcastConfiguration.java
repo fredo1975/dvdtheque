38b3b1da7b5914f1483057d5115880693f1b9eea
fredo1975 <fredo@BLUECHIPIT-1>
2019-11-01 12:02:04 +0100

    move hazelcast to service

--- dvdtheque-services/src/main/java/fr/fredos/dvdtheque/service/HazelcastConfiguration.java
+++ dvdtheque-services/src/main/java/fr/fredos/dvdtheque/service/HazelcastConfiguration.java
@@ -1,4 +1,4 @@
-package fr.fredos.dvdtheque.rest.conf;
+package fr.fredos.dvdtheque.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -8,13 +8,15 @@ import com.hazelcast.config.Config;
 import com.hazelcast.config.EvictionPolicy;
 import com.hazelcast.config.MapConfig;
 import com.hazelcast.config.MaxSizeConfig;
+
 @Configuration
 public class HazelcastConfiguration {
-	
+	@Value("${hazelcast.group.name}")
+	private String groupConfigName;
 	@Bean
 	public Config hazelCastConfig() {
 		Config config = new Config();
-		config.getGroupConfig().setName("hazelcast-dev");
+		config.getGroupConfig().setName(groupConfigName);
 		config.setInstanceName("hazelcast-instance").addMapConfig(new MapConfig().setName("films")
 				.setMaxSizeConfig(new MaxSizeConfig(200, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
 				.setEvictionPolicy(EvictionPolicy.LRU).setTimeToLiveSeconds(20));
