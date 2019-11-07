d16150c896ecb1dd9719539c00e64b1e5cd4a999
fredo1975 <fredo@BLUECHIPIT-1>
2019-11-07 22:37:43 +0100

    fix

--- dvdtheque-services/src/main/java/fr/fredos/dvdtheque/service/HazelcastConfiguration.java
+++ dvdtheque-services/src/main/java/fr/fredos/dvdtheque/service/HazelcastConfiguration.java
@@ -3,8 +3,6 @@ package fr.fredos.dvdtheque.service;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,7 +16,6 @@ import com.hazelcast.config.MaxSizeConfig;
 public class HazelcastConfiguration {
 	@Value("${hazelcast.group.name}")
 	private String groupConfigName;
-	protected Logger logger = LoggerFactory.getLogger(HazelcastConfiguration.class);
 	@Bean
 	public Config hazelCastConfig() {
 		Config config = new Config();
@@ -26,18 +23,13 @@ public class HazelcastConfiguration {
 		config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
 		config.getNetworkConfig().getJoin().getTcpIpConfig().setEnabled(true);
 		config.getNetworkConfig().getInterfaces().setEnabled(true);
-		logger.info("config.getNetworkConfig().getJoin().getTcpIpConfig().isEnabled()="+config.getNetworkConfig().getJoin().getTcpIpConfig().isEnabled());
-		logger.info("config.getNetworkConfig().getInterfaces().isEnabled()="+config.getNetworkConfig().getInterfaces().isEnabled());
 		List<String> interfaces = new ArrayList<>();
 		interfaces.add("192.168.1.*");
+		config.setProperty("hazelcast.initial.min.cluster.size","2");
 		config.getNetworkConfig().getInterfaces().setInterfaces(interfaces);
-		for(String in : config.getNetworkConfig().getInterfaces().getInterfaces()) { 
-			logger.info("Interface="+in);
-		}
-		//config.setProperty("hazelcast.initial.min.cluster.size","2");
 		config.setInstanceName("hazelcast-instance").addMapConfig(new MapConfig().setName("films")
 				.setMaxSizeConfig(new MaxSizeConfig(200, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
-				.setEvictionPolicy(EvictionPolicy.LRU).setTimeToLiveSeconds(20));
+				.setEvictionPolicy(EvictionPolicy.LRU).setTimeToLiveSeconds(200));
 		return config;
 	}
 }
