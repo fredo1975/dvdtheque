6cc7605a260c4055241281df77b7c60ee6140af5
fredo1975 <fredo@BLUECHIPIT-1>
2019-11-07 22:04:44 +0100

    fix

--- dvdtheque-services/src/main/java/fr/fredos/dvdtheque/service/HazelcastConfiguration.java
+++ dvdtheque-services/src/main/java/fr/fredos/dvdtheque/service/HazelcastConfiguration.java
@@ -3,6 +3,8 @@ package fr.fredos.dvdtheque.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,13 +18,19 @@ import com.hazelcast.config.MaxSizeConfig;
 public class HazelcastConfiguration {
 	@Value("${hazelcast.group.name}")
 	private String groupConfigName;
+	protected Logger logger = LoggerFactory.getLogger(HazelcastConfiguration.class);
 	@Bean
 	public Config hazelCastConfig() {
 		Config config = new Config();
 		config.getGroupConfig().setName(groupConfigName);
+		config.getNetworkConfig().getJoin().getTcpIpConfig().setEnabled(true);
+		logger.info("config.getNetworkConfig().getJoin().getTcpIpConfig().isEnabled()="+config.getNetworkConfig().getJoin().getTcpIpConfig().isEnabled());
 		List<String> interfaces = new ArrayList<>();
 		interfaces.add("192.168.1.*");
 		config.getNetworkConfig().getInterfaces().setInterfaces(interfaces);
+		for(String in : config.getNetworkConfig().getInterfaces().getInterfaces()) { 
+			logger.info("Interface="+in);
+		}
 		//config.setProperty("hazelcast.initial.min.cluster.size","2");
 		config.setInstanceName("hazelcast-instance").addMapConfig(new MapConfig().setName("films")
 				.setMaxSizeConfig(new MaxSizeConfig(200, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
