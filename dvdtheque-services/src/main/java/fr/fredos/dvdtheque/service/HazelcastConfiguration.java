c092665f27b3714f6bf7260c7df31c3130d29e4a
fredo1975 <fredo@BLUECHIPIT-1>
2019-11-07 23:32:38 +0100

    fix

--- dvdtheque-services/src/main/java/fr/fredos/dvdtheque/service/HazelcastConfiguration.java
+++ dvdtheque-services/src/main/java/fr/fredos/dvdtheque/service/HazelcastConfiguration.java
@@ -3,6 +3,7 @@ package fr.fredos.dvdtheque.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,7 +28,7 @@ public class HazelcastConfiguration {
 		List<String> interfaces = new ArrayList<>();
 		interfaces.add("192.168.1.*");
 		config.getNetworkConfig().getInterfaces().setInterfaces(interfaces);
-		config.setInstanceName("hazelcast-instance").addMapConfig(new MapConfig().setName("films")
+		config.setInstanceName(RandomStringUtils.random(8, true, false)).addMapConfig(new MapConfig().setName("films")
 				.setMaxSizeConfig(new MaxSizeConfig(200, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
 				.setEvictionPolicy(EvictionPolicy.LRU).setTimeToLiveSeconds(200));
 		return Hazelcast.newHazelcastInstance(config);
