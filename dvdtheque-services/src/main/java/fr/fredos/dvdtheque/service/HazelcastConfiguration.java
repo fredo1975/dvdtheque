0fb3ec15e5ccf86bdafefff41845b2f58d76e3ea
fredo1975 <fredo@BLUECHIPIT-1>
2019-11-07 23:01:12 +0100

    fix

--- dvdtheque-services/src/main/java/fr/fredos/dvdtheque/service/HazelcastConfiguration.java
+++ dvdtheque-services/src/main/java/fr/fredos/dvdtheque/service/HazelcastConfiguration.java
@@ -11,25 +11,29 @@ import com.hazelcast.config.Config;
 import com.hazelcast.config.EvictionPolicy;
 import com.hazelcast.config.MapConfig;
 import com.hazelcast.config.MaxSizeConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
 
 @Configuration
 public class HazelcastConfiguration {
 	@Value("${hazelcast.group.name}")
 	private String groupConfigName;
+	
+	
 	@Bean
-	public Config hazelCastConfig() {
+	public HazelcastInstance hazelcastInstance() {
 		Config config = new Config();
 		config.getGroupConfig().setName(groupConfigName);
-		config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
-		config.getNetworkConfig().getJoin().getTcpIpConfig().setEnabled(true);
+		//config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
+		//config.getNetworkConfig().getJoin().getTcpIpConfig().setEnabled(true);
 		config.getNetworkConfig().getInterfaces().setEnabled(true);
 		List<String> interfaces = new ArrayList<>();
-		interfaces.add("192.168.1.*");
-		config.setProperty("hazelcast.initial.min.cluster.size","2");
+		interfaces.add("192.168.1.1-24");
+		//config.setProperty("hazelcast.initial.min.cluster.size","2");
 		config.getNetworkConfig().getInterfaces().setInterfaces(interfaces);
 		config.setInstanceName("hazelcast-instance").addMapConfig(new MapConfig().setName("films")
 				.setMaxSizeConfig(new MaxSizeConfig(200, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
 				.setEvictionPolicy(EvictionPolicy.LRU).setTimeToLiveSeconds(200));
-		return config;
+		return Hazelcast.newHazelcastInstance(config);
 	}
 }
