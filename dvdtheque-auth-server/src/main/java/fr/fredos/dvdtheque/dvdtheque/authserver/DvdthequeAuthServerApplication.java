935ede6ea92128cbb0a6b3e8256f12b64243d9ca
fredo1975 <fredo@BLUECHIPIT-1>
2020-04-05 01:33:46 +0200

    init

---
+++ dvdtheque-auth-server/src/main/java/fr/fredos/dvdtheque/dvdtheque/authserver/DvdthequeAuthServerApplication.java
@@ -0,0 +1,11 @@
+package fr.fredos.dvdtheque.dvdtheque.authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvdthequeAuthServerApplication {
+	public static void main(String[] args) {
+        SpringApplication.run(DvdthequeAuthServerApplication.class, args);
+    }
+}
--- dvdtheque-auth-server/src/main/java/fr/fredos/dvdtheque/dvdtheque/authserver/OAuth2Config.java
+++ dvdtheque-auth-server/src/main/java/fr/fredos/dvdtheque/dvdtheque/authserver/OAuth2Config.java
@@ -1,4 +1,4 @@
-package fr.fredos.dvdtheque.dvdtheque_authserver;
+package fr.fredos.dvdtheque.dvdtheque.authserver;
 
 import javax.sql.DataSource;
 
@@ -6,15 +6,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 @Configuration
-//@EnableAuthorizationServer
-public class OAuth2Config /*extends AuthorizationServerConfigurerAdapter*/ {
+@EnableAuthorizationServer
+public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+	static final String CLIEN_ID = "dvdtheque-client";
 	@Autowired
 	private DataSource dataSource;
 	@Autowired
 	private AuthenticationManager authenticationManager;
-/*
+
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints.authenticationManager(this.authenticationManager).tokenStore(tokenStore())
@@ -39,5 +47,5 @@ public class OAuth2Config /*extends AuthorizationServerConfigurerAdapter*/ {
 	@Bean
 	public JdbcTokenStore tokenStore() {
 		return new JdbcTokenStore(dataSource);
-	}*/
+	}
 }
