267b111b10b739a833818afc23017c9b9a17d0e3
fredo1975 <fredo@BLUECHIPIT-1>
2020-05-03 22:55:46 +0200

    upgrade spring-boot to 2.2.6.RELEASE and spring cloud to Hoxton.SR4

--- dvdtheque-auth-server/src/test/java/fr/fredos/dvdtheque/dvdtheque/authorization/server/DvdthequeAuthServerApplicationTest.java
+++ dvdtheque-auth-server/src/test/java/fr/fredos/dvdtheque/dvdtheque/authorization/server/DvdthequeAuthServerApplicationTest.java
@@ -1,13 +1,9 @@
 package fr.fredos.dvdtheque.dvdtheque.authorization.server;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@ActiveProfiles("local")
-@RunWith(SpringRunner.class)
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class DvdthequeAuthServerApplicationTest {
 	@Test
