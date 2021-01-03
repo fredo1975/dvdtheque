c72f9b6d3d73c831128fdde3f6f4427628d6f4b0
fredo <fred@autogrif.com>
2021-01-03 19:50:17 +0100

    enhencements

---
+++ dvdtheque-resource-server/src/main/java/fr/fredos/dvdtheque/dvdtheque/resource/server/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,8 @@
+package fr.fredos.dvdtheque.dvdtheque.resource.server.configuration;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfiguration {
+
+}
