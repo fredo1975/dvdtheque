ee0070eac0160876221e05c4f0594e9f3bfb1acf
fredo <fred@autogrif.com>
2021-01-09 21:51:58 +0100

    enhencements

--- dvdtheque-resource-server/src/main/java/fr/fredos/dvdtheque/dvdtheque/resource/server/DvdthequeResourcesServerApplication.java
+++ dvdtheque-resource-server/src/main/java/fr/fredos/dvdtheque/dvdtheque/resource/server/DvdthequeResourcesServerApplication.java
@@ -2,23 +2,14 @@ package fr.fredos.dvdtheque.dvdtheque.resource.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DvdthequeResourcesServerApplication{
+	//private static final Logger LOG = LoggerFactory.getLogger(DvdthequeResourcesServerApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(DvdthequeResourcesServerApplication.class, args);
 	}
-/*
-	@GetMapping("/account")
-	public String getAccount() {
-		return "account 1";
-	}
-
-	@Bean
-	public ResourceServerTokenServices tokenService() {
-		RemoteTokenServices tokenServices = new RemoteTokenServices();
-		tokenServices.setClientId("b");
-		tokenServices.setClientSecret("b");
-		tokenServices.setCheckTokenEndpointUrl("http://localhost:9999/oauth/check_token");
-		return tokenServices;
-	}*/
 }
