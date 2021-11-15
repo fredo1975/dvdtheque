50f1f79a9260c7bc1b7e1bb80f16c7c4370a850a
fredo <fred@autogrif.com>
2021-11-15 08:45:37 +0100

    fix hazelcast multicast config

--- dvdtheque-services/src/main/java/fr/fredos/dvdtheque/service/config/HazelcastConfiguration.java
+++ dvdtheque-services/src/main/java/fr/fredos/dvdtheque/service/config/HazelcastConfiguration.java
@@ -1,34 +1,30 @@
 package fr.fredos.dvdtheque.service.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.commons.lang.RandomStringUtils;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import com.hazelcast.config.AutoDetectionConfig;
 import com.hazelcast.config.Config;
-import com.hazelcast.config.JoinConfig;
 import com.hazelcast.config.MapConfig;
+import com.hazelcast.config.MulticastConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 
 @Configuration
 @ComponentScan
 public class HazelcastConfiguration {
-	@Value("${hazelcast.networkconfig.interface}")
-	private String interfaces;
+	/*@Value("${hazelcast.networkconfig.interface}")
+	private String interfaces;*/
 	@Bean
 	public HazelcastInstance hazelcastInstance() {
 		Config config = new Config();
-		config.getNetworkConfig().getInterfaces().setEnabled(true);
-		List<String> interfacesList = new ArrayList<>();
-		interfacesList.add(interfaces);
-		config.getNetworkConfig().getInterfaces().setInterfaces(interfacesList);
-		config.getNetworkConfig().setJoin(new JoinConfig().setAutoDetectionConfig(new AutoDetectionConfig().setEnabled(false)));
+		config.getNetworkConfig().getInterfaces().setEnabled(false);
+		/*List<String> interfacesList = new ArrayList<>();
+		interfacesList.add(interfaces);*/
+		config.getNetworkConfig().getJoin().setMulticastConfig(new MulticastConfig().setEnabled(true));
+		//config.getNetworkConfig().getInterfaces().setInterfaces(interfacesList);
+		//config.getNetworkConfig().setJoin(new JoinConfig().setAutoDetectionConfig(new AutoDetectionConfig().setEnabled(false)));
 		config.setInstanceName(RandomStringUtils.random(8, true, false))
 				.addMapConfig(new MapConfig().setName("films"));
 						//.setMaxSizeConfig(new MaxSizeConfig(10000, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
