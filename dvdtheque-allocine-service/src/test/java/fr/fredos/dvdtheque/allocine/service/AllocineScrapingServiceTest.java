9b93c1f941d92c3f8767097da8583be4d301b05b
fredo <fred@autogrif.com>
2021-11-15 11:10:07 +0100

    fix test hazelcast conf

--- dvdtheque-allocine-service/src/test/java/fr/fredos/dvdtheque/allocine/service/AllocineScrapingServiceTest.java
+++ dvdtheque-allocine-service/src/test/java/fr/fredos/dvdtheque/allocine/service/AllocineScrapingServiceTest.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 
+import org.apache.commons.lang.RandomStringUtils;
 import org.jsoup.nodes.Element;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
@@ -15,16 +16,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import fr.fredos.dvdtheque.allocine.service.AllocineScrapingService;
+import com.hazelcast.config.AutoDetectionConfig;
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
+import com.hazelcast.config.MapConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+
 import fr.fredos.dvdtheque.service.IFilmService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {fr.fredos.dvdtheque.dao.Application.class,
 		fr.fredos.dvdtheque.service.ServiceApplication.class,
-		fr.fredos.dvdtheque.allocine.service.AllocineServiceApplication.class})
+		fr.fredos.dvdtheque.allocine.service.AllocineServiceApplication.class,
+		AllocineScrapingServiceTest.HazelcastConfiguration.class})
 @ActiveProfiles("test")
 public class AllocineScrapingServiceTest {
 	protected Logger logger = LoggerFactory.getLogger(AllocineScrapingServiceTest.class);
@@ -37,7 +47,17 @@ public class AllocineScrapingServiceTest {
 	public void setUp() throws Exception {
     	filmService.cleanAllFilms();
 	}
-    
+    @TestConfiguration
+	public static class HazelcastConfiguration {
+		@Bean
+		public HazelcastInstance hazelcastInstance() {
+			Config config = new Config();
+			config.getNetworkConfig().setJoin(new JoinConfig().setAutoDetectionConfig(new AutoDetectionConfig().setEnabled(false)));
+			config.setInstanceName(RandomStringUtils.random(8, true, false))
+					.addMapConfig(new MapConfig().setName("films"));
+			return Hazelcast.newHazelcastInstance(config);
+		}
+	}
     
     private void writeDoc(Element e) throws IOException {
     	String saveLocation = "D:\\tmp\\tmp.txt";
