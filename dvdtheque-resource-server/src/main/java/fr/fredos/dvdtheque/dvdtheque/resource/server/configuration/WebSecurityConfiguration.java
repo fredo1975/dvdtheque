ea7930b4aa3e1c40d6875629002cc7ea78fdc993
fredo <fred@autogrif.com>
2021-11-17 23:18:55 +0100

    remove useless projects

--- dvdtheque-resource-server/src/main/java/fr/fredos/dvdtheque/dvdtheque/resource/server/configuration/WebSecurityConfiguration.java
+++
@@ -1,8 +0,0 @@
-package fr.fredos.dvdtheque.dvdtheque.resource.server.configuration;
-
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfiguration {
-
-}
