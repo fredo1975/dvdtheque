fdde0617caf791062eb1d4df0c6ee35ca1f93e71
fredo <fred@autogrif.com>
2021-12-13 21:23:20 +0100

    fix

--- dvdtheque-tmdb-service/src/main/java/fr/fredos/dvdtheque/tmdb/TmdbServiceApplication.java
+++ dvdtheque-tmdb-service/src/main/java/fr/fredos/dvdtheque/tmdb/TmdbServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class TmdbServiceApplication {
-	public static void main(String[] args){
+	public static void main(String... args){
         SpringApplication.run(TmdbServiceApplication.class,args);
     }
 }
--- dvdtheque-tmdb-service/src/main/java/fr/fredos/dvdtheque/tmdb/service/TmdbService.java
+++
@@ -1,7 +0,0 @@
-package fr.fredos.dvdtheque.tmdb.service;
-
-import fr.fredos.dvdtheque.common.tmdb.model.Results;
-
-public interface TmdbService {
-	//Results replaceFilm(final Long tmdbId) throws Exception;
-}
