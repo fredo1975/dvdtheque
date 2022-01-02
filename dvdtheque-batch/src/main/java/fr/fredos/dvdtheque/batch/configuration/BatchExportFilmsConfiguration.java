b44cfa077c3fd179581daa7e79075a17241790cb
fredo <fred@autogrif.com>
2022-01-02 12:48:06 +0100

    fix

--- dvdtheque-batch/src/main/java/fr/fredos/dvdtheque/batch/configuration/BatchExportFilmsConfiguration.java
+++ dvdtheque-batch/src/main/java/fr/fredos/dvdtheque/batch/configuration/BatchExportFilmsConfiguration.java
@@ -26,8 +26,6 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.ParameterizedTypeReference;
