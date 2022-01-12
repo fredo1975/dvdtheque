72cad9446d07388a09dc488f9bb23780b6947ecd
fredo <fred@autogrif.com>
2022-01-12 18:27:12 +0100

    add log to test

--- dvdtheque-allocine-service/src/main/java/fr/fredos/dvdtheque/allocine/controller/ScheduledTasks.java
+++ dvdtheque-allocine-service/src/main/java/fr/fredos/dvdtheque/allocine/controller/ScheduledTasks.java
@@ -1,5 +1,7 @@
 package fr.fredos.dvdtheque.allocine.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -8,12 +10,14 @@ import fr.fredos.dvdtheque.allocine.service.AllocineService;
 
 @Component
 public class ScheduledTasks {
+	protected Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
 	@Autowired
     AllocineService allocineService;
 	
 	
 	@Scheduled(cron = "${fichefilm.parsing.cron}", zone = "Europe/Paris")
 	public void retrieveAllocineScrapingFicheFilm() {
+		logger.info("retrieveAllocineScrapingFicheFilm");
 		allocineService.scrapAllAllocineFicheFilm();
 	}
 	
