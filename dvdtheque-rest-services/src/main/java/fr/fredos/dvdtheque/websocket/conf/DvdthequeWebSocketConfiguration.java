c12f156b97f9bda4e97423bac750bd57f2b2cce3
fredo <fred@autogrif.com>
2022-01-31 23:54:09 +0100

    fix

--- dvdtheque-rest-services/src/main/java/fr/fredos/dvdtheque/websocket/conf/DvdthequeWebSocketConfiguration.java
+++ dvdtheque-rest-services/src/main/java/fr/fredos/dvdtheque/websocket/conf/DvdthequeWebSocketConfiguration.java
@@ -1,8 +1,5 @@
 package fr.fredos.dvdtheque.websocket.conf;
 
-import java.util.Optional;
-
-import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,14 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.messaging.simp.stomp.StompCommand;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-import org.springframework.messaging.support.ChannelInterceptor;
-import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
