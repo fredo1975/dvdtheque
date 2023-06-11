878c6210e4db59a57e6c62e5016320553f2b860d
fredo1975 <fred@autogrif.com>
2023-06-11 23:36:59 +0200

    upgrade http client timeout

--- dvdtheque-rest-services/src/main/java/fr/fredos/dvdtheque/rest/config/ContextConfiguration.java
+++ dvdtheque-rest-services/src/main/java/fr/fredos/dvdtheque/rest/config/ContextConfiguration.java
@@ -1,7 +1,12 @@
 package fr.fredos.dvdtheque.rest.config;
 
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.AbstractOAuth2Token;
@@ -26,6 +31,25 @@ public class ContextConfiguration {
 		    request.getHeaders().setBearerAuth(token.getTokenValue());
 		    return execution.execute(request, body);
 		});
+		rest.setRequestFactory(requestFactory());
 		return rest;
+    }
+	
+	private HttpComponentsClientHttpRequestFactory requestFactory() {
+
+        RequestConfig requestConfig = RequestConfig
+            .custom()
+            .setConnectionRequestTimeout(2000)
+            .setSocketTimeout(2000)
+            .setConnectTimeout(200000)
+            .build();
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(5);
+        connectionManager.setDefaultMaxPerRoute(5);
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                                                          .setConnectionManager(connectionManager)
+                                                          .setDefaultRequestConfig(requestConfig)
+                                                          .build();
+        return new HttpComponentsClientHttpRequestFactory(httpClient);
     }
 }
--- dvdtheque-tmdb-service/src/main/java/fr/fredos/dvdtheque/tmdb/config/ContextConfiguration.java
+++ dvdtheque-tmdb-service/src/main/java/fr/fredos/dvdtheque/tmdb/config/ContextConfiguration.java
@@ -1,13 +1,38 @@
 package fr.fredos.dvdtheque.tmdb.config;
 
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ContextConfiguration {
 	@Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+		return new RestTemplateBuilder()
+	            .requestFactory(this::requestFactory)
+	            .build();
+    }
+	private HttpComponentsClientHttpRequestFactory requestFactory() {
+
+        RequestConfig requestConfig = RequestConfig
+            .custom()
+            .setConnectionRequestTimeout(2000)
+            .setSocketTimeout(2000)
+            .setConnectTimeout(200000)
+            .build();
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(5);
+        connectionManager.setDefaultMaxPerRoute(5);
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                                                          .setConnectionManager(connectionManager)
+                                                          .setDefaultRequestConfig(requestConfig)
+                                                          .build();
+        return new HttpComponentsClientHttpRequestFactory(httpClient);
     }
 }
