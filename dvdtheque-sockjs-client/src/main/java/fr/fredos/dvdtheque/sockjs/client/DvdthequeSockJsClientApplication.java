299ee12ddad4163f4133f9a276aeb7ee5e8a7288
fredo1975 <fred@autogrif.com>
2024-01-07 23:13:01 +0100

    add new project dvdtheque-sockjs-client

---
+++ dvdtheque-sockjs-client/src/main/java/fr/fredos/dvdtheque/sockjs/client/DvdthequeSockJsClientApplication.java
@@ -0,0 +1,11 @@
+package fr.fredos.dvdtheque.sockjs.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvdthequeSockJsClientApplication {
+	public static void main(String... args) {
+		SpringApplication.run(DvdthequeSockJsClientApplication.class, args);
+	}
+}
