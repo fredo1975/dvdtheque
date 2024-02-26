f970cfbae05834e984c1abcca9b5f91c407913de
fredo1975 <fred@autogrif.com>
2024-02-27 00:39:24 +0100

    fix websocket issue

--- dvdtheque-rest-services/src/main/java/fr/fredos/dvdtheque/rest/config/WebSecurityConfig.java
+++ dvdtheque-rest-services/src/main/java/fr/fredos/dvdtheque/rest/config/WebSecurityConfig.java
@@ -5,14 +5,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -21,26 +19,11 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtGra
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSocketSecurity
-public class OAuth2ClientSecurityConfig {
-	protected Logger logger = LoggerFactory.getLogger(OAuth2ClientSecurityConfig.class);
-
-	/*
-	@Bean
-    AuthorizationManager<Message<?>> messageAuthorizationManager(MessageMatcherDelegatingAuthorizationManager.Builder messages) {
-        return messages.simpSubscribeDestMatchers("/topic").permitAll().build();
-        
-    }*/
-
+@EnableWebSecurity
+public class WebSecurityConfig {
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf(csrf -> csrf
-                // ignore our stomp endpoints since they are protected using Stomp headers
-                .ignoringRequestMatchers("/dvdtheque-ws/**"))
-		.headers(headers -> headers
-                .frameOptions(frameOptions -> frameOptions
-                        .sameOrigin()))
-		.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated())
+		http.authorizeHttpRequests((authz) -> authz.requestMatchers("/dvdtheque-ws/**").permitAll().anyRequest().authenticated())
 	       .oauth2ResourceServer(resourceServerConfigurer -> resourceServerConfigurer
                 .jwt(jwtConfigurer -> jwtConfigurer
                         .jwtAuthenticationConverter(jwtAuthenticationConverter())));
