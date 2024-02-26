746e1d310fc05ea36d51de89ae4333f377001cc0
fredo1975 <fred@autogrif.com>
2024-02-26 23:28:20 +0100

    some fixes

--- dvdtheque-rest-services/src/test/java/fr/fredos/dvdtheque/rest/config/TestWebSocketConfig.java
+++ dvdtheque-rest-services/src/test/java/fr/fredos/dvdtheque/rest/config/TestWebSocketConfig.java
@@ -1,12 +1,9 @@
 package fr.fredos.dvdtheque.rest.config;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
-@Configuration
 @EnableWebSocketMessageBroker
 public class TestWebSocketConfig implements WebSocketMessageBrokerConfigurer{
 	private static final String STOMP_ENDPOINT = 	"dvdtheque-ws";
@@ -15,12 +12,13 @@ public class TestWebSocketConfig implements WebSocketMessageBrokerConfigurer{
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/"+STOMP_ENDPOINT)
 		.setAllowedOriginPatterns("*")
-		.withSockJS();
+		.withSockJS()
+		.setWebSocketEnabled(true);
 	}
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
-		registry.enableSimpleBroker("/queue/","/topic/");
+		registry.enableSimpleBroker("/topic");
 		registry.setApplicationDestinationPrefixes("/app");
 	}
 }
